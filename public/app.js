e8b4d2c33ffb39baeef4ebf09fd2de86b71af63c
wlboucher <wlboucher@wisc.edu>
2024-04-23 17:45:11 -0500

    updating header

--- public/app.js
+++ public/app.js
@@ -1,3 +1,38 @@
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+const signbtns = document.getElementById("signbtns");
+
+// Function to manage visibility of sign-up/login buttons based on menu state and screen width
+function manageButtonVisibility() {
+    if (window.innerWidth <= 1022) { // Assumes 768px is the breakpoint for mobile view
+        if (document.getElementById('navbarBasicExample').classList.contains('is-active')) {
+            signbtns.classList.remove('is-hidden'); // Show buttons when menu is active
+        } else {
+            signbtns.classList.add('is-hidden'); // Hide buttons when menu is not active
+        }
+    } else {
+        signbtns.classList.add('is-hidden'); // Always hide buttons on larger screens
+    }
+}
+
+$navbarBurgers.forEach(el => {
+    el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+        // Manage visibility of sign buttons based on current menu state and screen width
+        manageButtonVisibility();
+    });
+});
+
+// Add event listener for resizing to adjust visibility based on screen width
+window.addEventListener('resize', manageButtonVisibility);
+
+
+
 function r_e(id) {
   if (!id) {
     console.error("Invalid ID provided:", id);
@@ -1328,11 +1363,11 @@ document.querySelector(".eventsfooter").addEventListener("click", () => {
 });
 
 document.querySelector(".pointfooter").addEventListener("click", () => {
-  r_e("pointbtn").click();
+  r_e("pointbtn1").click();
 });
 
 // points page content
-r_e("pointbtn").addEventListener("click", () => {
+r_e("pointbtn1").addEventListener("click", () => {
   let check_auth = auth.currentUser;
   console.log("btn clicked");
   if (check_auth == null) {
