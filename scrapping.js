b0486a0a183b0ff81fc296ddc17024389ef84314
agrawal35 <agrawal35@wisc.edu>
2024-04-30 13:48:52 -0500

    logging into member log in

--- scrapping.js
+++ scrapping.js
@@ -1,43 +1,40 @@
-// import puppeteer
+const puppeteer = require("puppeteer");
 
-const puppeteer = require("puppeteer")
+async function go() {
+  // Launch browser
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 100,
+  });
+  const page = await browser.newPage();
 
-async function go(){
-    // cause the browser to launch
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 100,
-    })
-    const page = await browser.newPage();
+  // Visit the site to be tested
+  await page.goto("https://amabadgers.web.app/");
 
-    // visist the site to be tested
+  // Click on the burger menu button
+  await page.click("body > header > nav > div.navbar-brand > a");
 
-    await page.goto("https://amabadgers.web.app/")
+  // Wait for a short delay for the menu to open (adjust this delay if necessary)
+  await page.waitForSelector("#burgerloginbtn");
 
-    // public view of pages
-    // view the whole home page
-    // scrolling goes to footer submit button
-    const footersubmit = await page.$("#submit_btn");
-    await page.evaluate(el => {
-        const scrollIntoViewSlowly = (footersubmit) => {
-            const scrollStep = 10;
-            const scrollInterval = 30;
-            let intervalId = setInterval(() => {
-                const rect = footersubmit.getBoundingClientRect();
-                if (rect.top <= window.innerHeight) {
-                    clearInterval(intervalId);
-                } else {
-                    window.scrollBy(0, scrollStep);
-                }
-            }, scrollInterval);
-        };
-        scrollIntoViewSlowly(el);
-    }, footersubmit);
+  // Click on the button with the selector #burgerloginbtn
+  await page.click("#burgerloginbtn");
 
+  // Wait for the email input field to be visible
+  await page.waitForSelector("#email");
 
+  // Type "test@test.com" into the email input field
+  await page.type("#email", "test@test.com");
 
-    // close the browser
-    // browser.close()
+  // Type "testtest" into the password input field
+  await page.type("#password", "testtest");
+
+  // Click on the submit button
+  await page.click("#submit");
+
+  // Close the browser
+  await browser.close();
 }
-// calll go
+
+// Call the go function
 go();
